c8ca0f2d547e473a25b69610290a159dad386851
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2022-12-14 15:51:16 +0000

    Added icon

--- src/script.js
+++ src/script.js
@@ -21,17 +21,21 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute ("src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute ("alt", response.data.weather[0].description);
 }
 
 let apiKey ="89a9e2a51e094f98264a0f24cd6d562e";
-let city = "Paris";
+let city = "New York";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log (apiUrl);
 axios.get(apiUrl).then(displayTemperature);
