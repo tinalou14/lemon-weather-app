45ee3923e8e5ee009d4842f926997b48762d6aaf
tinalou14 <117734587+tinalou14@users.noreply.github.com>
2022-12-14 16:28:10 +0000

    Added the search engine

--- src/script.js
+++ src/script.js
@@ -34,8 +34,21 @@ function displayTemperature(response) {
     iconElement.setAttribute ("alt", response.data.weather[0].description);
 }
 
+function search(city){
 let apiKey ="89a9e2a51e094f98264a0f24cd6d562e";
-let city = "New York";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
